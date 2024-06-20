3930918e2c7b50f4be7bb7cb3a3a2295943d8fff
Reytler Souza <reytlercompare@hotmail.com>
2024-06-20 12:03:20 -0300

    Configuração do React Router

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,13 +1,24 @@
 import './App.css'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
+import { Route, Routes } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { Login } from './Pages/Login'
+import { Home } from './Pages/Home'
+import { NotFound } from './Pages/NotFound'
 
 const queryClient = new QueryClient()
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <h1>Dados PC MT</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
