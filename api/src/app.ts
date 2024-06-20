42e11c80f906d47864ec1d0741d68a0808c0defa
Reytler Souza <reytlercompare@hotmail.com>
2024-06-20 16:59:42 -0300

    Corrigindo erro de cors

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,10 +1,17 @@
 import bodyParser from 'body-parser'
 import express from 'express'
+import cors from 'cors'
 
 import userRoutes from './routes/user'
 
 const app = express()
 
+const corsOptions = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+}
+
+app.use(cors(corsOptions))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
