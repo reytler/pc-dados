297607cdad5fcbdd76067bf2108fe9b6ef93ca1b
Reytler Souza <reytlercompare@hotmail.com>
2024-06-21 14:43:00 -0300

    Criação do hook useToken

---
+++ front/src/Hooks/useToken.ts
@@ -0,0 +1,14 @@
+import Cookies from 'js-cookie'
+import { useJwt } from 'react-jwt'
+import { Idecoded } from '../Pages/Login'
+
+export function useToken() {
+  const { decodedToken, isExpired } = useJwt<string>(
+    `${Cookies.get('token')?.toString()}`,
+  )
+
+  //@ts-ignore
+  const decoded: Idecoded = decodedToken
+
+  return { decoded }
+}
